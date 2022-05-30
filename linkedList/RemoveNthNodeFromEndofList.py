e6816696e4601c43be5ea41d8a0deb377355b3eb
Rucha Deshpande <rucha.d1690@gmail.com>
2022-05-30 07:17:54 +0530

    Update RemoveNthNodeFromEndofList.py

--- linkedList/RemoveNthNodeFromEndofList.py
+++ linkedList/RemoveNthNodeFromEndofList.py
@@ -1,15 +1,17 @@
-def removeNth(head, val):
-    dummy=ListNode(0)
-    dummy=head
-    slow=fast=dummy
-
-    for _ in range(val):
-        slow=slow.next
-
-    while fast and fast.next:
-        slow=slow.next
-        fast=fast.next
-
-    fast.next=fast.next.next
-
-    return dummy.next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy =  ListNode(0, head)
+        
+        curr=slow=dummy
+        
+        for _ in range(n):
+            curr = curr.next
+        
+        while curr and curr.next:
+            curr = curr.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
+        
+        return dummy.next
+        
